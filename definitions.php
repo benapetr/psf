d71ecc5ad48ee548cd7a71ef193f8b335b6a24ae
Petr Bena <benapetr@gmail.com>
2015-05-07 10:42:01 -0400

    version info

---
+++ definitions.php
@@ -0,0 +1,6 @@
+<?php
+
+define ('PSF_VERSION_MAJOR', '1');
+define ('PSF_VERSION_MINOR', '0');
+define ('PSF_VERSION_REVISION', '0');
+define ('PSF_VERSION', PSF_VERSION_MAJOR . "." . PSF_VERSION_MINOR . "." . PSF_VERSION_REVISION);
---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+require(dirname(__FILE__) . "/definitions.php");
+
+function psf_version()
+{
+    return PSF_VERSION;
+}
