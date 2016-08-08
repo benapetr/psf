2bc3208dd2a05cf5b48d6bbcf8f96d1c7d35e744
Petr Bena <benapetr@gmail.com>
2016-08-08 10:44:29 +0200

    use Open Sans as default for google fonts

--- includes/googlefonts.php
+++ includes/googlefonts.php
@@ -22,4 +22,5 @@ function googlefonts_init($page)
     $open_sans = 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,700';
     if (!in_array($open_sans, $page->ExternalCss))
         $page->ExternalCss[] = $open_sans;
+    $page->Style->items["*"]["font-family"] = "Open Sans";
 }
