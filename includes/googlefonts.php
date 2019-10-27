57e28a93f5b188dd1d366f0ca3938e9e4885f6b5
Petr Bena <benapetr@gmail.com>
2019-10-27 17:32:19 +0100

    use https google font

--- includes/googlefonts.php
+++ includes/googlefonts.php
@@ -19,7 +19,7 @@ if (!defined("PSF_ENTRY_POINT"))
 
 function googlefonts_init($page)
 {
-    $open_sans = 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,700';
+    $open_sans = 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700';
     if (!in_array($open_sans, $page->ExternalCss))
         $page->ExternalCss[] = $open_sans;
     $page->Style->items["*"]["font-family"] = "Open Sans";
