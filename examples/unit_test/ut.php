a9bc12637e7eec87268830c5a0248ebe1137ba91
Petr Bena <benapetr@gmail.com>
2020-04-19 00:07:17 +0200

    unit test

---
+++ examples/unit_test/ut.php
@@ -0,0 +1,18 @@
+<?php
+
+require ("../../psf.php");
+require ("../../includes/unit_test/ut.php");
+
+$ut = new UnitTest();
+
+$ut->Evaluate("xx", false);
+$ut->Evaluate("x2", true);
+
+echo ("\n\n");
+
+$ut->PrintResults();
+
+if ($ut->IsPassed())
+    exit(0);
+
+exit(1);
