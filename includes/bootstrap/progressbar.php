e48f28ec3dc676c38042bfb75e0330d78e615db5
Petr Bena <benapetr@gmail.com>
2025-02-09 11:08:22 +0100

    BS_ProgressBar SetValue

--- includes/bootstrap/progressbar.php
+++ includes/bootstrap/progressbar.php
@@ -35,17 +35,19 @@ class BS_ProgressBar extends DivContainer
         parent::__construct($_parent);
     }
 
+    //! Sets the value of the progress bar, if the value is out of range, it will be adjusted
+    public function SetValue($_value)
+    {
+        if ($_value < $this->Min)
+            $this->Value = $this->Min;
+        else if ($_value > $this->Max)
+            $this->Value = $this->Max;
+        else
+            $this->Value = $_value;
+    }
+
     public function ToHtml()
     {
         return '<div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="' . $this->Value . '" aria-valuemin="' . $this->Min . '" aria-valuemax="' . $this->Max . '" style="width:' . $this->Value . '%">' . $this->Text . '</div></div>';
     }
 }
-
-/*
-<div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="70"
-  aria-valuemin="0" aria-valuemax="100" style="width:70%">
-    <span class="sr-only">70% Complete</span>
-  </div>
-</div> 
-*/
